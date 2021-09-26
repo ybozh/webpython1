3da1e0d683738ba4eb5d0105825deb3c90c58441
Yuriy <to.yuriy@gmail.com>
2021-09-26 12:45:02 +0300

    commit_html

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!!!'
+    # return 'Hello World!!! Python'
+    name = 'Rosalia'
+    return render_template('index.html', title='Welcome', username=name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
