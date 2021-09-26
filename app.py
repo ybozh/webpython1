3ae0730c1c11959d2af9b984d7e5030460e8322e
Yuriy <to.yuriy@gmail.com>
2021-09-26 12:51:12 +0300

    commit4

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     # return 'Hello World!!! Python'
-    name = 'Rosalia'
+    name = 'Yuriy'
     return render_template('index.html', title='Welcome', username=name)
 
 if __name__ == '__main__':
