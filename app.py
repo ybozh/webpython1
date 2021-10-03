d450b376756a785c648279b8b4b334d3f107ce20
Yuriy <to.yuriy@gmail.com>
2021-10-03 09:43:17 +0300

    com5

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask.helpers import url_for
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
     # return 'Hello World!!! Python'
+    # s = Request.form.get("TEXT_1")
+    # a = Request.form.get()
     name = 'Yuriy'
-    a = 1 + 4
-    return render_template('index.html', title='Welcome', username=name, a=a)
+    e = 3
+    return render_template('index.html', title=e, username=name)
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr = user))
+    else:
+        return render_template("login.html")
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
