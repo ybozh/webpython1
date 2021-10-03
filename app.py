438cea63dc0774c951aacca0b60648f61a68562b
Yuriy <to.yuriy@gmail.com>
2021-10-03 22:24:51 +0300

    com10

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def hello_world():
     # return 'Hello World!!! Python'
     # s = Request.form.get("TEXT_1")
     # a = Request.form.get()
-    name = 'Yuriy'
+    name = 'Yurii'
     e = 3
     return render_template('index.html', title=e, username=name)
 
@@ -23,9 +23,21 @@ def login():
     else:
         return render_template("login.html")
 
+
+
+@app.route("/form", methods=["POST", "GET"])
+def form():
+    if request.method == "POST":
+        sum = int(request.form["TEXT_2"]) + int(request.form["TEXT_1"])
+        return redirect(url_for("user", usr = sum))
+    else:
+        return render_template("form.html")
+
+
 @app.route("/<usr>")
 def user(usr):
     return f"<h1>{usr}</h1>"
+    # return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- form.py
+++ form.py
@@ -1,1 +1,6 @@
-print("Form!")
+def form():
+    if request.method == "POST":
+        sum = request.form["TEXT_1"]
+        return redirect(url_for("user", usr = sum))
+    else:
+        return render_template("form.html")
